e388ea9cce71ae8673bf619edd8b9866a4869751
Hobie Li <hobbylee6@gmail.com>
2022-10-21 13:51:43 -0600

    init android project (bottom navigation activity); setup Robotium;

---
+++ src/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.CMPUT301F22T01.foodbit.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
