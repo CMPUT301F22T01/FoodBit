b27dcdd1664cc3149972218ef24875d6e798d50d
Hobie Li <hobbylee6@gmail.com>
2022-10-21 17:30:07 -0600

    setup bottom navigation bar; setup four basic fragments

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/MainActivity.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/MainActivity.java
@@ -1,9 +1,15 @@
 package com.CMPUT301F22T01.foodbit;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.CMPUT301F22T01.foodbit.ui.IngredientStorageFragment;
+import com.CMPUT301F22T01.foodbit.ui.MealPlanFragment;
+import com.CMPUT301F22T01.foodbit.ui.RecipeBookFragment;
+import com.CMPUT301F22T01.foodbit.ui.ShoppingCartFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,27 +17,56 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.CMPUT301F22T01.foodbit.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarItemView;
+import com.google.android.material.navigation.NavigationBarMenuView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    IngredientStorageFragment ingredientStorageFragment = IngredientStorageFragment.newInstance("1","2");
+    RecipeBookFragment recipeBookFragment = RecipeBookFragment.newInstance("1","2");
+    MealPlanFragment mealPlanFragment = MealPlanFragment.newInstance("1","2");
+    ShoppingCartFragment shoppingCartFragment = ShoppingCartFragment.newInstance("1","2");
+
+//    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        final NavigationBarView navView = findViewById(R.id.nav_view);
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_ingredient_storage:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, ingredientStorageFragment).commit();
+                        return true;
+
+                    case R.id.navigation_recipe_book:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, recipeBookFragment).commit();
+                        return true;
+
+                    case R.id.navigation_meal_plan:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, mealPlanFragment).commit();
+                        return true;
+
+                    case R.id.navigation_shopping_cart:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, shoppingCartFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+
+
+
+
 
 }
