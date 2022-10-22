afedbd62da264a9834de3845ab3bf99dbe9edf6a
Hobie Li <hobbylee6@gmail.com>
2022-10-21 21:37:08 -0600

    basic steps on implementing top bar; set default destination as meal plan

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/MainActivity.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/MainActivity.java
@@ -3,6 +3,7 @@ package com.CMPUT301F22T01.foodbit;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.CMPUT301F22T01.foodbit.ui.IngredientStorageFragment;
 import com.CMPUT301F22T01.foodbit.ui.MealPlanFragment;
@@ -22,23 +23,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: set default page
 
         final NavigationBarView navView = findViewById(R.id.nav_view);
+
+        // set and init default destination
+        final Fragment defaultFragment = mealPlanFragment;
+        final int defaultFragmentLayout = R.id.fragment_meal_plan;
+        navView.setSelectedItemId(defaultFragmentLayout);
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_container, defaultFragment).commit();
+
         navView.setOnItemSelectedListener(item -> {
             // TODO: move this into a controller class
             int itemId = item.getItemId();
             if (itemId == R.id.fragment_ingredient_storage) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, ingredientStorageFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_container, ingredientStorageFragment).commit();
                 return true;
             } else if (itemId == R.id.fragment_recipe_book) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, recipeBookFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_container, recipeBookFragment).commit();
                 return true;
             } else if (itemId == R.id.fragment_meal_plan) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, mealPlanFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_container, mealPlanFragment).commit();
                 return true;
             } else if (itemId == R.id.fragment_shopping_cart) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_activity_main, shoppingCartFragment).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_container, shoppingCartFragment).commit();
                 return true;
             }
             return false;
