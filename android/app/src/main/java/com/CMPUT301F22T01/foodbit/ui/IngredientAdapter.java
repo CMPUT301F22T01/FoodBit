dd05dee2d01bf0f257a8ad61850331d0e0c7c784
hr-jackson <hrjackso@ualberta.ca>
2022-11-01 10:07:08 -0600

    adding more documentation notes along with removing unused import statements in ingredient classes

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/IngredientAdapter.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/IngredientAdapter.java
@@ -28,6 +28,11 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.Vi
     private final ArrayList<Ingredient> items;
     private final int mode;
 
+    /**
+     * Adapter for the ingredient items
+     * @param items list of ingredient items
+     * @param mode view to be used
+     */
     public IngredientAdapter(ArrayList<Ingredient> items, int mode) {
         this.items = items;
         this.mode = mode;
@@ -45,11 +50,17 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.Vi
         private final TextView ingredientAmount;
         private final TextView ingredientUnit;
 
+        /**
+         * Choosing which view to use
+         * @param view the view to be used
+         * @param mode deciding which view to be used
+         */
         public ViewHolder(View view, int mode) {
             super(view);
             // todo: Define click listener for the ViewHolder's View
             //view.setOnClickListener();
             switch(mode) {
+                // used to view ingredient details when an item in ingredient storage is clicked on
                 case INGREDIENT_STORAGE: {
                     view.setOnClickListener(v -> {
                         Bundle bundle = new Bundle();
@@ -89,6 +100,12 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.Vi
         }
     }
 
+    /**
+     * Inflating the layout
+     * @param parent context from parent
+     * @param viewType the view to be displayed
+     * @return
+     */
     @NonNull
     @Override
     public IngredientAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -97,6 +114,13 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.Vi
         return new IngredientAdapter.ViewHolder(view, mode);
     }
 
+    /**
+     * Setting the views for ingredient details that are initially shown
+     * Includes the ingredients description, amount, and unit
+     * If more details want to be viewed, the ingredient must be clicked on
+     * @param holder holder for the view
+     * @param position ingredient details to be presented
+     */
     @Override
     public void onBindViewHolder(@NonNull IngredientAdapter.ViewHolder holder, int position) {
         // get value of each fields
@@ -117,6 +141,10 @@ public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.Vi
         unitView.setText(unit);
     }
 
+    /**
+     * Amount of items determined by the size
+     * @return number of items
+     */
     @Override
     public int getItemCount() {
         return items.size();
