cb00925fa57e93eaccb5eced929119d90825e184
a-s-ahmed <asahmed2@ualberta.ca>
2022-11-03 20:25:25 -0600

    attempting to setup separate users

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/MainActivity.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/MainActivity.java
@@ -1,7 +1,9 @@
 package com.CMPUT301F22T01.foodbit;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
@@ -15,9 +17,12 @@ import com.CMPUT301F22T01.foodbit.controllers.MealPlanController;
 //import com.CMPUT301F22T01.foodbit.ui.MealPlanFragment;
 //import com.CMPUT301F22T01.foodbit.ui.RecipeBookFragment;
 //import com.CMPUT301F22T01.foodbit.ui.ShoppingCartFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.installations.FirebaseInstallations;
 
 
 import java.util.List;
@@ -29,17 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
 
     public final static String TAG = "MainActivity";
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    static FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    final CollectionReference recipeBookRef = db.collection("Recipe Book");
+    static CollectionReference recipeBookRef = db.collection("Recipe Book");
     public static RecipeBook recipeBook = new RecipeBook();
-    final CollectionReference ingredientStorageRef = db.collection("ingredient list");
+    static CollectionReference ingredientStorageRef = db.collection("ingredient list");
     public static IngredientStorage ingredientStorage = new IngredientStorage();
 
 
     // access a Cloud Firestore instance and retrieve data
-    final CollectionReference mealPlanRef = db.collection("Meals");
+    static CollectionReference mealPlanRef = db.collection("Meals");
     public static MealPlanController mealPlan = new MealPlanController();
+
+    static String FID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +65,39 @@ public class MainActivity extends AppCompatActivity {
         NavHostFragment navHostFragment = (NavHostFragment) supportFragmentManager.findFragmentById(R.id.nav_container);
         assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
+//        NavController.OnDestinationChangedListener(new On)
         BottomNavigationView bottomNav = findViewById(R.id.nav_bar);
         NavigationUI.setupWithNavController(bottomNav, navController);
+
+    }
+
+    private void assignUserDB() {
+        // [START get_installation_id]
+        moveTaskToBack(true);
+        FirebaseInstallations.getInstance().getId()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (task.isSuccessful()) {
+                            Log.e("Installations", "Installation ID: " + task.getResult());
+                            FID = task.getResult();
+//                            dbConnected = true;
+//                            notifyAll();
+                            recipeBookRef = db.collection(FID).document().collection("Recipe Book");
+                            ingredientStorageRef = db.collection(FID).document().collection("ingredient list");
+                            mealPlanRef = db.collection(FID).document().collection("Meals");
+//                            mealPlan = new MealPlanController();
+                        } else {
+                            Log.e("Installations", "Unable to get Installation ID");
+//                            recipeBookRef = db.collection(FID).document().collection("Recipe Book");
+//                            ingredientStorageRef = db.collection(FID).document().collection("ingredient list");
+//                            mealPlanRef = db.collection(FID).document().collection("Meals");
+//                            mealPlan =
+                        }
+                    }
+                });
+        // [END get_installation_id]
     }
 
 }
+
