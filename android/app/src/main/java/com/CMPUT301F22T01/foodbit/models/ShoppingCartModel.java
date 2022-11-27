c7e84928cb9a615d12ea86ecd917055d723c2100
Fanbang Yang <y39564569@gmail.com>
2022-11-27 10:49:41 -0700

    update commit for Picked an ingredient

---
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/models/ShoppingCartModel.java
@@ -0,0 +1,28 @@
+package com.CMPUT301F22T01.foodbit.models;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.logging.Filter;
+
+public class ShoppingCartModel extends ViewModel {
+    private final MutableLiveData<Set<Filter>> filters = new MutableLiveData<>();
+
+    private final LiveData<ArrayList<Ingredient>> originalList = ...;
+    private final LiveData<ArrayList<Ingredient>> filteredList = ...;
+
+    public LiveData<ArrayList<Ingredient>> getFilteredList() {
+        return filteredList;
+    }
+
+    public LiveData<Set<Filter>> getFilters() {
+        return filters;
+    }
+
+    public void addFilter(Filter filter) { ... }
+
+    public void removeFilter(Filter filter) { ... }
+}
