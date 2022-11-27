d61f9be0e7750d905211307ce3920956969d4e8e
hr-jackson <hrjackso@ualberta.ca>
2022-11-27 13:31:26 -0700

    adding in javadocs

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/models/IngredientLocation.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/models/IngredientLocation.java
@@ -2,19 +2,35 @@ package com.CMPUT301F22T01.foodbit.models;
 
 import java.io.Serializable;
 
+/**
+ * Class to represent location for ingredients
+ * Categories can have a name and an id
+ */
 public class IngredientLocation implements Serializable, dbObject {
     private String locationName;
     private String id;
 
+    /**
+     * Required empty constructor
+     */
     public IngredientLocation() {
         // Empty constructor
     }
 
+    /**
+     * Creating an IngredientLocation object that can have a name and an id
+     * @param locationName name of the location
+     * @param id id of the location
+     */
     public IngredientLocation(String locationName, String id) {
         this.locationName = locationName;
         this.id = id;
     }
 
+    /**
+     * Creating an IngredientLocation object that can have a name
+     * @param locationName name of the location
+     */
     public IngredientLocation(String locationName) {
         this.locationName = locationName;
     }
