e915103f587c5c82f7528bded016c5dfa48273ae
Hobie Li <hobbylee6@gmail.com>
2022-11-26 21:02:01 -0700

    Fixed recipes ui test

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/IRecipe.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/IRecipe.java
@@ -8,6 +8,6 @@ public interface IRecipe {
      * Get ingredient list of a recipe
      * @return map of ingredient id's and the number of ingredients you need to make this recipe
      */
-    Map<String, Float> getIngredientList();
+    Map<String, Float> doGetIngredientList();
 
 }
