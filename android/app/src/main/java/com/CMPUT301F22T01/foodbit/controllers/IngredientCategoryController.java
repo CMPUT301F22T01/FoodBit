4268593c4ed9a81164a2e34aeaed84c286bb4739
hr-jackson <hrjackso@ualberta.ca>
2022-11-27 11:40:45 -0700

    adding in javadocs

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/controllers/IngredientCategoryController.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/controllers/IngredientCategoryController.java
@@ -15,27 +15,50 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Stores each of the categories used in ingredients
+ */
 public class IngredientCategoryController implements Serializable {
     private DatabaseController db = new DatabaseController("Categories");
     private final ArrayList<IngredientCategory> categories;
 
+    /**
+     * Creates a new list of IngredientCategory items
+     */
     public IngredientCategoryController() {
         categories = new ArrayList<IngredientCategory>();
     }
 
+    /**
+     * Creates a new array list for the ingredient category options
+     * @param categories a list of IngredientCategory
+     */
     public IngredientCategoryController(List<IngredientCategory> categories) {
         this.categories = (ArrayList<IngredientCategory>) categories;
     }
 
+    /**
+     * Allows categories to be retrieved
+     * @return list of the categories
+     */
     public ArrayList<IngredientCategory> getCategories() {
         return categories;
     }
+
+    /**
+     * Allows categories to be set
+     * @param categories the array list of categories to be set
+     */
 
     public void setCategories(ArrayList<IngredientCategory> categories) {
         this.categories.clear();
         this.categories.addAll(categories);
     }
 
+    /**
+     * Allows retrieving of the description/name of each category
+     * @return list of the category names
+     */
     public List<String> getCategoryDescription() {
         List<String> list = new ArrayList<>();
         for (IngredientCategory category : categories) {
@@ -44,22 +67,38 @@ public class IngredientCategoryController implements Serializable {
         return list;
     }
 
+    /**
+     * Check if a category is in the list
+     * @param category the category to be checked
+     * @return if the category is in categories or not
+     */
     public boolean contains(IngredientCategory category) {
         return categories.contains(category);
     }
 
+    /**
+     * Allows a new category to be added
+     * @param category category to be added
+     */
     public void add(IngredientCategory category) {
         String TAG = "IngredientCategoryAddCategory";
         assert !contains(category) : "This category already exists!";
         db.addItem(category);
     }
 
+    /**
+     * Allows an existing category to be deleted
+     * @param category the category to be deleted
+     */
     public void delete(IngredientCategory category) {
         String TAG = "IngredientCategoryDeleteCategory";
         assert contains(category) : "this category does not exist!!";
         db.deleteItem(category);
     }
 
+    /**
+     * Loading all of the categories from the database
+     */
     public void loadAllFromDB() {
         categories.clear();
         CollectionReference collectionReference = MainActivity.categoryListRef;
