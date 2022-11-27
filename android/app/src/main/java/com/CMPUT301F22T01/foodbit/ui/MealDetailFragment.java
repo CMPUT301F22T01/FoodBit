771b646178711c5484673b5b1188d9775f2e550e
a-s-ahmed <asahmed2@ualberta.ca>
2022-11-26 20:27:07 -0700

    Add meal consume button

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/MealDetailFragment.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/MealDetailFragment.java
@@ -21,10 +21,13 @@ import android.widget.TextView;
 import com.CMPUT301F22T01.foodbit.MainActivity;
 import com.CMPUT301F22T01.foodbit.R;
 import com.CMPUT301F22T01.foodbit.controllers.MealPlanController;
+import com.CMPUT301F22T01.foodbit.models.Ingredient;
 import com.CMPUT301F22T01.foodbit.models.MealPlan;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class MealDetailFragment extends Fragment {
 
@@ -109,24 +112,32 @@ public class MealDetailFragment extends Fragment {
                 fm.executePendingTransactions();
                 newFragment.show(fm, "EditMeal");
 
-                //TODO: Not working, Attempting to update details page after editing
-//                newFragment.getDialog().setOnDismissListener(new DialogInterface.OnDismissListener() {
-//                    @Override
-//                    public void onDismiss(DialogInterface dialog) {
-//                        // reset text
-//                        mealPlan = newFragment.getUpdatedMeal();
-//                        descriptionView.setText(mealPlan.getName());
-//                        SimpleDateFormat sf = new SimpleDateFormat("MMM dd/yy");
-//                        mealDateView.setText(sf.format(mealPlan.getDate()));
-//                        servingsView.setText(String.valueOf(mealPlan.getServings()));
-//                    }
-//                });
             };
 
 //                return true;
 
         });
 
+        //ate button
+        Button ateButton = view.findViewById(R.id.button_meal_ate);
+        ateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(consumeIngredients(mealPlan)) {
+                    MainActivity.mealPlan.deleteMeal(mealPlan);
+                    Snackbar snackbar = Snackbar.make(getActivity().findViewById(R.id.nav_container),
+                            "Meal consumed! Corresponding ingredients will be removed from your" +
+                                    "ingredient list!", Snackbar.LENGTH_SHORT);
+                    snackbar.setAnchorView(R.id.nav_bar).show();
+                    Navigation.findNavController(v).popBackStack();
+                } else{
+                    Snackbar snackbar = Snackbar.make(getActivity().findViewById(R.id.nav_container),
+                            "Not enough ingredients within storage!", Snackbar.LENGTH_SHORT);
+                    snackbar.setAnchorView(R.id.nav_bar).show();
+                }
+
+            }
+        });
 
         return view;
     }
@@ -144,6 +155,41 @@ public class MealDetailFragment extends Fragment {
         int position = getArguments().getInt("position");
         mealPlan = MainActivity.mealPlan.getMealByPosition(position);
         Log.d(TAG, String.valueOf(mealPlan));
+    }
+
+    /**
+     * Check if we have enough ingredients to consume the meal
+     * @return true if enough, false if not.
+     */
+    public boolean checkIngredients(MealPlan meal) {
+        ArrayList<Ingredient> mealIngredients = meal.getIngredients();
+        for (int i = 0; i <mealIngredients.size(); i++) {//check all ingredients in this meal
+            Ingredient temp = MainActivity.ingredientStorage.getIngredientById(mealIngredients.get(i).getId());
+            if (temp!=null) {//Ingredient exists
+                if (temp.getAmount() < mealIngredients.get(i).getAmount()) {//Not enough.
+                    return false;
+                }
+            } else {//Ingredient doesn't exist
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Consume ingredients if we have enough ingredients.
+     * @return true if enough and consumed, false if not.
+     */
+    public boolean consumeIngredients(MealPlan meal) {
+        if(checkIngredients(meal)){ //we have enough so consume the ingredients!
+            ArrayList<Ingredient> mealIngredients = meal.getIngredients();
+            for (int i = 0; i <mealIngredients.size(); i++) { //reduce the amount within each ingredient
+                Ingredient temp = MainActivity.ingredientStorage.getIngredientById(mealIngredients.get(i).getId());
+                temp.setAmount(temp.getAmount() - mealIngredients.get(i).getAmount());
+            }
+            return true;
+        }
+        return false; //checkIngredients false therefore we don't have enough
     }
 
     @NonNull
