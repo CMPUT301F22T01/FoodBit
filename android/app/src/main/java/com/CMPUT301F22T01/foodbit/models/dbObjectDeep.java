38f1dbd2ad2ed5a098cb0d045a1d47d22adc6eb5
a-s-ahmed <asahmed2@ualberta.ca>
2022-11-27 13:21:51 -0700

    Refactor dbObjectCustom to be more descriptive.

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/models/dbObjectDeep.java
+++
@@ -1,7 +0,0 @@
-package com.CMPUT301F22T01.foodbit.models;
-
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-
-public interface dbObjectDeep {
-    dbObjectDeep createFromDoc(QueryDocumentSnapshot t);
-}
