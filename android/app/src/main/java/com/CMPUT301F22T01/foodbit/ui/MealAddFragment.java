6a7610f08e2aa5e61c4249e6af86804553833357
a-s-ahmed <asahmed2@ualberta.ca>
2022-11-28 00:48:34 -0700

    Make meal detail,add,edit pages different for Ingredients vs Recipes.

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/MealAddFragment.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/MealAddFragment.java
@@ -156,6 +156,15 @@ public class MealAddFragment extends DialogFragment {
                         if(str.compareTo(temp) == 0) {
                             mealSelected = temp;
                             positionSelected = itemsDropdown.indexOf(mealSelected);
+                            if(positionSelected < ingredientList.size()) {
+                                if (ingredientList.get(positionSelected).getUnit() != null) {
+                                    servingsEditText.setHint(ingredientList.get(positionSelected).getUnit());
+                                    servingsLayout.setHint(ingredientList.get(positionSelected).getUnit());
+                                }
+                            } else {
+                                servingsEditText.setHint("Servings");
+                                servingsLayout.setHint("Servings");
+                            }
                             return;
                         } else {
                             positionSelected = i+1;
@@ -166,12 +175,6 @@ public class MealAddFragment extends DialogFragment {
             }
         });
 
-//        mealAddTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//
-//            }
-//        });
 
         //Date picker
         mealDateEditText = (EditText) view.findViewById(R.id.meal_add_date);
