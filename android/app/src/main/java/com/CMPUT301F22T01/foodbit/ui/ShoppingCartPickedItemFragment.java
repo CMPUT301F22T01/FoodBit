090d7ca45afb4d0f19b0f6a44dbd2696ca8ad628
Fanbang Yang <y39564569@gmail.com>
2022-11-27 17:57:56 -0700

    update commit for Picked an ingredient

--- android/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/ShoppingCartPickedItemFragment.java
+++ android/app/src/main/java/com/CMPUT301F22T01/foodbit/ui/ShoppingCartPickedItemFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -35,9 +36,10 @@ public class ShoppingCartPickedItemFragment extends DialogFragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String TAG = "Picked Ingredient";
 
-    private Context context;
-    private IngredientController ingredientController;
+    public Context context;
+    public IngredientController ingredientController;
     public Ingredient ingredient;
+    public int position;
 
     MaterialToolbar topBar;
     TextView description;
@@ -50,10 +52,6 @@ public class ShoppingCartPickedItemFragment extends DialogFragment {
 
     public ShoppingCartPickedItemFragment() {
         // Required empty public constructor
-    }
-
-    public ShoppingCartPickedItemFragment(Ingredient ingredient) {
-        this.ingredient = ingredient;
     }
 
     @Override
@@ -79,6 +77,7 @@ public class ShoppingCartPickedItemFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getIngredient();
         // set the style of the dialog fragment to be full screen
         setStyle(DialogFragment.STYLE_NORMAL, R.style.Theme_FoodBit_FullScreenDialog);
     }
@@ -87,6 +86,38 @@ public class ShoppingCartPickedItemFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shopping_cart_edit, container, false);
+        View view =  inflater.inflate(R.layout.fragment_shopping_cart_edit, container, false);
+
+        topBar = view.findViewById(R.id.shopping_pick_top_bar);
+        description = view.findViewById(R.id.shopping_picked_description);
+        amountNeeded = view.findViewById(R.id.shopping_picked_need_amount);
+        unit = view.findViewById(R.id.shopping_picked_unit);
+        category = view.findViewById(R.id.shopping_picked_category);
+
+        topBar.setTitle("Picked Ingredient");
+        // close button
+        topBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).popBackStack();
+            }
+        });
+
+        // setting to current ingredient details
+        description.setText(ingredient.getDescription());
+        amountNeeded.setText(String.valueOf(ingredient.getAmount()));
+        unit.setText(ingredient.getUnit());
+        category.setText(ingredient.getCategory());
+
+
+
+        return view;
+    }
+
+    private void getIngredient() {
+        assert getArguments() != null;
+        position = getArguments().getInt("position");
+        ingredient = MainActivity.ingredientController.getIngredientByPosition(position);
+        Log.d(TAG, String.valueOf(ingredient));
     }
 }
